77256007066b02f8cc360177ff700288a850dcff
pantho0 <panthomashreky6@gmail.com>
2023-11-27 16:55:56 +0600

    banner section added

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,11 +1,18 @@
 
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <MainLayout></MainLayout>
+      element: <MainLayout></MainLayout>,
+      children : [
+        {
+          path: "/",
+          element : <Home></Home>
+        }
+      ]
     },
   ]);
 
