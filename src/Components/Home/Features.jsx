e83eeb4a5872a4ec9abf27e2b23c2ff180935c47
pantho0 <panthomashreky6@gmail.com>
2023-11-27 19:44:39 +0600

    features page added

---
+++ src/Components/Home/Features.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { GiDoorway } from "react-icons/gi";
+
+const Features = () => {
+  const [features, setFeatures] = useState([]);
+  useEffect(() => {
+    fetch("features.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setFeatures(data);
+      });
+  }, []);
+  console.log(features);
+  return (
+    <div className="space-y-20 my-20">
+      <div className="max-w-fit mx-auto">
+        <h2 className="font-ubuntu text-5xl text-center pb-8 text-[#3b0032] border-b-2 border-[#3b0032] border-dotted">
+          Our Features
+        </h2>
+      </div>
+      <div className="flex justify-evenly">
+        {features.map((feature) => (
+          <div
+            key={feature.title}
+            className="overflow-hidden rounded-lg shadow-lg w-80"
+          >
+            <img className="w-full h-[200px] object-cover" src={feature.icons} alt="" />
+            <div className="p-4 flex flex-col gap-2">
+              <h3 className="text-xl font-semibold text-center">
+                {feature.title}
+              </h3>
+              <div className="text-sm text-justify">
+                <p>{feature.description}</p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <div className="badge badge-secondary bg-[#3b0032] border-none">
+                  Total Orders:
+                </div>
+                <div className="badge badge-secondary bg-[#3b0032] border-none">
+                  Total Deliveries:
+                </div>
+                <div className="badge badge-secondary bg-[#3b0032] border-none">
+                  Total Users:
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Features;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,32 +1,16 @@
 import React from "react";
 import Container from "../../Layouts/Container/Container";
-import Banner from '../../../public/banner.jpg'
+import BannerSection from "../../Components/Home/BannerSection";
+import Features from "../../Components/Home/Features";
 
 const Home = () => {
   return (
-    <Container>
-      <div
-        className="hero min-h-[70vh]"
-        style={{
-            backgroundImage: `url(${Banner})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            objectFit:'cover'
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-white">
-          <div className="max-w-md">
-            <h1 className="mb-2 text-5xl font-bold">Ship Anywhere, Anytime with Ease</h1>
-            <p className="mb-5">
-            Simplify Shipping with Our Online Platform
-            </p>
-            <input type="text" placeholder="Search Your Service Here" className="input input-bordered input-md w-full max-w-xs mr-1" />
-            <button className="btn px-8 text-white bg-[#3b0032] border-none hover:text-[#F7FF00] hover:bg-[#3b0032d7]">Search</button>
-          </div>
-        </div>
-      </div>
-    </Container>
+    <>
+      <BannerSection></BannerSection>
+      <Container>
+        <Features></Features>
+      </Container>
+    </>
   );
 };
 
